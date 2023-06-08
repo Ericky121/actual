ce575e57d68ce54c15cdd24fc95accc6ff73d77b
Ericky <ericky121@gmail.com>
2023-06-08 16:21:59 +0800

    6/8 상세페이지 작업1

---
+++ lib/product/component/product_card.dart
@@ -0,0 +1,61 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              'asset/img/food/ddeok_bok_gi.jpg',
+              width: 110,
+              height: 110,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            width: 16.0,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '떡볶이',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  '전통 떡볶이의 정석! 원하는대로 맵기를 선택하고 추억의 떡볶이맛에 빠져보세요! 쫀득한 쌀떡과 말랑한 오뎅의 완벽한 조화! 잘익은 반숙 계란은 덤!',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: BODY_TEXT_COLOR,
+                    fontSize: 14.0,
+                  ),
+                ),
+                Text(
+                  '￦1000',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: PRIMARY_COLOR,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
